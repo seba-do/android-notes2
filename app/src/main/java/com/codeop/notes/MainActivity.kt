10eb1b4c6a42ad7fd05105a7df7e7498361bc5ab
seba <seba.domma@gmail.com>
2022-02-03 18:06:18 +0100

    Add switchting of fragments to the Activity

--- app/src/main/java/com/codeop/notes/MainActivity.kt
+++ app/src/main/java/com/codeop/notes/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.codeop.notes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.codeop.notes.databinding.ActivityMainBinding
+import com.codeop.notes.view.AddFragment
+import com.codeop.notes.view.ListFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        switchToListFragment()
+    }
+
+    private fun switchToAddFragment() {
+        supportFragmentManager.commit {
+            replace(binding.fragmentHolder.id, AddFragment(::switchToListFragment))
+        }
+    }
+
+    private fun switchToListFragment() {
+        supportFragmentManager.commit {
+            replace(binding.fragmentHolder.id, ListFragment(::switchToAddFragment))
+        }
     }
 }
